825ce7508cb5cc80dd22b6aa575158e01fb1e102
juan cuevas <juannmcuevas97@gmail.com>
2022-02-08 23:43:25 -0300

    homework05Finished

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -90,7 +90,7 @@ function verificarPassword(usuario, password) {
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
-  // Tu código:
+  // Tu código: 
   if (usuario['password'] === password){
     return true;
   } else {
